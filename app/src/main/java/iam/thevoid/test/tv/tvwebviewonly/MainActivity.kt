a638fd561eb5c3564fe1d4f0ce793ec4ea1e5fb9
iam <alesetar@gmail.com>
2022-12-27 16:12:30 +0300

    default project

---
+++ app/src/main/java/iam/thevoid/test/tv/tvwebviewonly/MainActivity.kt
@@ -0,0 +1,11 @@
+package iam.thevoid.test.tv.tvwebviewonly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
